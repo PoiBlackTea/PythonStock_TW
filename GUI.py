213282e0e320dc54c6eb4486d144ace13971d367
LAI, WEI-CHEN <kill630459@gmail.com>
2020-02-18 22:29:47 +0800

    second version

--- GUI.py
+++ GUI.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'test.ui'
+# Form implementation generated from reading ui file 'c:\Users\kill6\Desktop\python coding env\untitled.ui'
 #
-# Created by: PyQt5 UI code generator 5.11.2
+# Created by: PyQt5 UI code generator 5.13.2
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(750, 600)
+        MainWindow.resize(800, 592)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
@@ -25,45 +27,40 @@ class Ui_MainWindow(object):
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setObjectName("label_2")
         self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_2.setObjectName("lineEdit_2")
-        self.gridLayout.addWidget(self.lineEdit_2, 1, 1, 1, 1)
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setObjectName("comboBox")
+        self.gridLayout.addWidget(self.comboBox, 1, 1, 1, 1)
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setObjectName("label_3")
         self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
-        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_3.setObjectName("lineEdit_3")
-        self.gridLayout.addWidget(self.lineEdit_3, 2, 1, 1, 1)
-        self.lineEdit_4 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_4.setObjectName("lineEdit_4")
-        self.gridLayout.addWidget(self.lineEdit_4, 3, 1, 1, 1)
+        self.comboBox_2 = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox_2.setObjectName("comboBox_2")
+        self.gridLayout.addWidget(self.comboBox_2, 2, 1, 1, 1)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.gridLayout.addWidget(self.lineEdit_2, 3, 1, 1, 1)
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
         self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
         self.gridLayout.addWidget(self.tableWidget, 4, 1, 1, 1)
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setObjectName("pushButton")
         self.gridLayout.addWidget(self.pushButton, 5, 1, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.pushButton.clicked.connect(self.back)
+        self.pushButton.clicked.connect(self.button)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "證券小工具 ver1.2 By Sheep"))
+        MainWindow.setWindowTitle(_translate(
+            "MainWindow", "證券小工具 ver2.0 By Sheep"))
         self.label.setText(_translate("MainWindow", "證券代號"))
-        self.label_2.setText(_translate("MainWindow", "From"))
-        self.label_3.setText(_translate("MainWindow", "To"))
+        self.label_2.setText(_translate("MainWindow", "起始日期"))
+        self.label_3.setText(_translate("MainWindow", "終止日期"))
         self.pushButton.setText(_translate("MainWindow", "OK"))
-
-
-        
-
--- Stock.py
+++ Stock.py
@@ -1,141 +1,155 @@
 # -*- coding: utf-8 -*-
-from GUI import Ui_MainWindow
-from PyQt5.QtWidgets import QMainWindow,QApplication,QTableWidget,QTableWidgetItem
-import thread_return as thread
-import requests
-import re 
+import re
 import sys
-import timeit
-import os
+from traceback import print_exc
 
-def arithmetic(li1, li2):
+import requests
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QTableWidget,
+                             QTableWidgetItem)
+
+import thread_return as thread
+from Stock_GUI import Ui_MainWindow
+
+
+def str2obj(s, s1=';', s2='='):
+    li = s.split(s1)
+    res = {}
+    for kv in li:
+        li2 = kv.split(s2)
+        if len(li2) > 1:
+            res[li2[0]] = li2[1]
+    return res
+
+
+def Calculate_stock(li1, li2):
+
     ar0 = []
-    try:
-        if len(li1) == 17 and len(li2) != 17:
-            length = 17
-            li1[15][1] = '0'
-            li1[15][0] = '差異數調整'
-            li2.insert(15,['差異數調整', '0', '0', '0.00'])
-        elif len(li2)==17 and len(li1)!=17:          
-            length = 17
-            li2[15][1] = '0'
-            li2[15][0] = '差異數調整'
-            li1.insert(15,['差異數調整', '0', '0', '0.00'])
-        else:
-            length = len(li1)
-        
-        for i in range(length):
-            ar1 = []
-            for j in range(4):
-                if j == 0:
-                    tmp = li1[i][j]
-                    ar1.append(tmp)
-                elif j == 1 or j == 2:
-                    tmp = str(int(li1[i][j]) - int(li2[i][j]))
-                    if len(tmp) < 4 or ('-' in tmp and len(tmp) == 4):
-                        tmp = tmp
-                    else:
-                        tmp = tmp[:-3] + ',' + tmp[-3:]
-                    tmp = tmp + "(" + li2[i][j] + "->" + li1[i][j] + ")"
-                    ar1.append(tmp)
-                else:
-                    tmp = str(round(float(li1[i][j]) - float(li2[i][j]), 2)) + "(" + li2[i][j] + "->" + li1[i][j] + ")"
-                    ar1.append(tmp)
-            ar0.append(ar1)
-        
-    except:
-        self.lineEdit_4.setText("check  arithmetic func and run again")
+    if len(li1) == 17 and len(li2) != 17:
+        li1[15][0:2] = ['差異數調整', '0']
+        li2.insert(15, ['差異數調整', '0', '0', '0.00'])
+    elif len(li2) == 17 and len(li1) != 17:
+        li2[15][0:2] = ['差異數調整', '0']
+        li1.insert(15, ['差異數調整', '0', '0', '0.00'])
+    elif len(li2) == 17 and len(li1) == 17:
+        li1[15][0:2] = ['差異數調整', '0']
+        li2[15][0:2] = ['差異數調整', '0']
+    table_length = len(li1)
+
+    for i in range(table_length):
+        ar1 = []
+        for j in range(4):
+            if j == 0:
+                tmp = li1[i][j]
+            elif j == 1 or j == 2:
+                tmp = str(int(li1[i][j].replace(',', '')) -
+                          int(li2[i][j].replace(',', '')))
+                tmp = f'{int(tmp):,} ({li2[i][j]}->{li1[i][j]})'
+            else:
+                tmp = str(round(float(li1[i][j]) - float(li2[i][j]), 2))
+                tmp = f'{float(tmp)} ({li2[i][j]}->{li1[i][j]})'
+            ar1.append(tmp)
+        ar0.append(ar1)
 
     return ar0
 
-# preprcessing
-def map_split(string):
-    result = ''.join(re.findall('>.+<', string)).replace('<', '').split('>')
 
-    return [result[2], result[3].replace(',', ''), result[4].replace(',', ''), result[5]]
-    
-#  Webscraping
-def Webscraping(date,ID):
+def Web_scraping(date, ID):
     try:
         url = "https://www.tdcc.com.tw/smWeb/QryStockAjax.do"
-        data = {'scaDates' : '', 'scaDate' : '', 'SqlMethod' : 'StockNo', 'tockNo' : '', 'radioStockNo' : '', 'StockName' : '', 'REQ_OPR' : 'SELECT', 'clkStockNo' : '', 'clkStockName' : '' }
-        data['scaDates'] = data['scaDate'] = date
-        data['tockNo'] = data['radioStockNo'] = data['clkStockNo'] = ID
-        html = requests.post(url, data)
-        pattern = '<td align=\"center\">.+</td>\s+<td align=\"center\">.+</td>\s+<td align=\"right\">.+</td>\s+<td align=\"right\">.+</td>\s+<td align=\"right\">.+</td>'
+        data = {'scaDates': date, 'scaDate': date, 'SqlMethod': 'StockNo', 'tockNo': ID, 'radioStockNo': ID,
+                'StockName': '', 'REQ_OPR': 'SELECT', 'clkStockNo': ID, 'clkStockName': ''}
+        html = requests.post(url, params=data)
+        if html.status_code == requests.codes.ok:
+            pattern = r'<td align=\"center\">.+</td>\s+<td align=\"right\">.+</td>\s+<td align=\"right\">.+</td>\s+<td align=\"right\">.+</td>'
+            list_table = re.findall(pattern, html.text)
+        else:
+            raise ValueError
 
-        """ 
-        html form:
-           <tr >
-              <td align="center">1</td>
-              <td align="center">1-999</td>
-              <td align="right">109,373</td>
-              <td align="right">29,070,387</td>
-              <td align="right">0.94</td>
-            </tr>
-        """
-        # regular regression
-        reg = re.findall(pattern, html.text)
-        if reg == []:
-            html = requests.post(url, data)
-            reg = re.findall(pattern, html.text)
-        
-        #get clear data
-        stock_li = list(map(map_split, reg))
-
+        stock_li = list(map(lambda x: re.findall(r'>(.*)<', x), list_table))
+    except ValueError:
+        print('html.status_code error')
     except:
-        self.lineEdit_4.setText('Html run error')
-        raise
-	
+        print_exc
     return stock_li
 
-# GUI
+
 class MainWindow(QMainWindow, Ui_MainWindow):
+
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-        
-    def back(self):
-        token1 =  self.lineEdit.text()     # 股票號碼
-        token2 =  self.lineEdit_2.text()   # 起始日期
-        token3 =  self.lineEdit_3.text()   # 截止日期
+        self.func_load_Date()
+        self.comboBox.activated[str].connect(self.func_change_start)
+        self.comboBox_2.activated[str].connect(self.func_change_end)
 
-        """ 
-        call thread_return.py 
-        thread_1 and thread_2  catch two information ,respectively.
-        """
-        thread_1 = thread.ThreadWithReturnValue(target = Webscraping, args = (token3, token1))
-        thread_2 = thread.ThreadWithReturnValue(target = Webscraping, args = (token2, token1))
-        thread_1.start()
-        thread_2.start()
-        stock_li1 = thread_1.join()
-        stock_li2 = thread_2.join()
+    def button(self):
 
-        # get reslut
-        final = arithmetic(stock_li1, stock_li2)
-            
-        # plane
-        self.tableWidget.setColumnCount(4)
-        self.tableWidget.setRowCount(len(final))
-        horizontalHeader = ["持股/單位數分級", "人數", "股數/單位數", "佔集保庫存數比例 (%)"]
-        self.tableWidget.setHorizontalHeaderLabels(horizontalHeader)    
-        self.lineEdit_4.setText('ID:' + token1 + ' From:' + token2 + ' To:' + token3)
-        count = 0
-            
-        # push result to plane
-        for i in final:
-            self.tableWidget.setItem(count, 0, QTableWidgetItem(i[0]))
-            self.tableWidget.setItem(count, 1, QTableWidgetItem(i[1]))
-            self.tableWidget.setItem(count, 2, QTableWidgetItem(i[2]))
-            self.tableWidget.setItem(count, 3, QTableWidgetItem(i[3]))
-            count += 1
+        try:
+            Stock_number = self.lineEdit.text()
+            if not Stock_number:
+                raise ValueError("股票號碼不可為空")
+            elif not Stock_number.isdigit():
+                raise ValueError("股票號碼必須是數字")
+        except ValueError as e:
+            self.lineEdit_2.setText(str(e))
+        else:
+            thread_1 = thread.ThreadWithReturnValue(
+                target=Web_scraping, args=(self.val_end_date, Stock_number))
+            thread_2 = thread.ThreadWithReturnValue(
+                target=Web_scraping, args=(self.val_start_date, Stock_number))
+            thread_1.start()
+            thread_2.start()
+            stock_li1 = thread_1.join()
+            stock_li2 = thread_2.join()
 
-        self.tableWidget.resizeColumnsToContents()
-        self.tableWidget.resizeRowsToContents()
-        
+            final = Calculate_stock(stock_li1, stock_li2)
+
+            self.tableWidget.setColumnCount(4)
+            self.tableWidget.setRowCount(len(final))
+            horizontalHeader = ["持股/單位數分級", "人數", "股數/單位數", "佔集保庫存數比例 (%)"]
+            self.tableWidget.setHorizontalHeaderLabels(horizontalHeader)
+            self.lineEdit_2.setText(
+                f'證券代號: {Stock_number} 起始日期:{self.val_start_date} 終止日期:{self.val_end_date}')
+
+            count = 0
+            for i in final:
+                self.tableWidget.setItem(count, 0, QTableWidgetItem(i[0]))
+                self.tableWidget.setItem(count, 1, QTableWidgetItem(i[1]))
+                self.tableWidget.setItem(count, 2, QTableWidgetItem(i[2]))
+                self.tableWidget.setItem(count, 3, QTableWidgetItem(i[3]))
+                count += 1
+
+            self.tableWidget.resizeColumnsToContents()
+            self.tableWidget.resizeRowsToContents()
+
+    def func_load_Date(self):
+
+        headers = '''POST /smWeb/QryStockAjax.do HTTP/1.1\nHost: www.tdcc.com.tw\nConnection: keep-alive\nAccept: application/json, text/javascript, */*; q=0.01\nSec-Fetch-Dest: empty\nX-Requested-With: XMLHttpRequest\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36\nContent-Type: application/x-www-form-urlencoded;charset=UTF-8\nOrigin: https://www.tdcc.com.tw\nSec-Fetch-Site: same-origin\nSec-Fetch-Mode: cors\nReferer: https://www.tdcc.com.tw/smWeb/QryStock.jsp\nAccept-Encoding: gzip, deflate, br\nAccept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,ja;q=0.5\n'''
+        headers = str2obj(headers, '\n', ': ')
+        params = {'REQ_OPR': 'qrySelScaDates'}
+        url = "https://www.tdcc.com.tw/smWeb/QryStockAjax.do"
+        date = requests.post(url, params=params, headers=headers)
+        if date.status_code == requests.codes.ok:
+            date_li = re.findall(r'\d+', date.text)
+            self.comboBox.addItems(date_li)
+            self.comboBox_2.addItems(date_li)
+            self.val_start_date = self.val_end_date = date_li[0]
+        else:
+            self.lineEdit_2.setText('Loading Date Fail')
+
+    def func_change_start(self, text):
+        self.val_start_date = text
+
+    def func_change_end(self, text):
+        self.val_end_date = text
+
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
-    os.system("pause")
+
+    try:
+        app = QApplication(sys.argv)
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec_())
+    except:
+        print_exc
